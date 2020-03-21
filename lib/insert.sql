c06dd36545b728f49ed6c629d0cc8d8d4109b39c
Michael Fahim <michael_fahim@yahoo.com>
2020-03-21 17:57:51 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Bomba", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2,"Ma7she", 10, "M", "black", "intense", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,"Borqana", 6, "F", "dark brown", "Nice", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"Fesa", 19, "M", "Green", "Slimy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5,"Bolbola", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6,"ElHendi", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7,"Karsa", 6, "F", "Blue", "naive", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8,null, 20, "M", "black", "aggressive", 0);
